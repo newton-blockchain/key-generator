8244a1f5ed6a03f2e7c1e21e65c155837358b472
tolya-yanot <tolyayanot@gmail.com>
2021-07-08 23:57:30 +0300

    get keys from exist file

--- index.js
+++ index.js
@@ -3,23 +3,36 @@ const TonWeb = require("tonweb");
 const fs = require("fs");
 
 async function init() {
+    const nacl = TonWeb.utils.nacl;
+    const web3 = new Web3();
+
     const FILE_NAME = 'keys.json';
 
-    if (fs.existsSync(FILE_NAME)) {
-        console.log(`File ${FILE_NAME} already exists`);
-        return;
-    }
+    let tonSecretKey, ethAccount;
+    let needSave = false;
 
-    console.log('Generating TON and ETH keys..');
+    if (fs.existsSync(FILE_NAME)) {
+        console.log(`Getting keys from ${FILE_NAME}..`);
+        const json = JSON.parse(fs.readFileSync(FILE_NAME).toString('utf-8'));
+
+        if (!json.tonPrivateKey) throw new Error('invalid ' + FILE_NAME);
+        if (!json.ethPrivateKey) throw new Error('invalid ' + FILE_NAME);
+
+        tonSecretKey = json.tonPrivateKey;
+        ethAccount = web3.eth.accounts.privateKeyToAccount(json.ethPrivateKey);
+
+    } else {
+        console.log('Generating TON and ETH keys..');
+        const newKeyPair = nacl.box.keyPair();
+
+        tonSecretKey = TonWeb.utils.bytesToHex(newKeyPair.secretKey);
+        ethAccount = web3.eth.accounts.create();
+        needSave = true;
+    }
 
     // TON
 
-    const nacl = TonWeb.utils.nacl;
-    const newKeyPair = nacl.box.keyPair();
-    const newPublicKey = TonWeb.utils.bytesToHex(newKeyPair.publicKey);
-    const newSecretKey = TonWeb.utils.bytesToHex(newKeyPair.secretKey);
-
-    const keyPair = nacl.sign.keyPair.fromSeed(TonWeb.utils.hexToBytes(newSecretKey));
+    const keyPair = nacl.sign.keyPair.fromSeed(TonWeb.utils.hexToBytes(tonSecretKey));
     const tonweb = new TonWeb(new TonWeb.HttpProvider());
     const walletClass = tonweb.wallet.all['v3R2'];
     const wallet = new walletClass(tonweb.provider, {
@@ -27,26 +40,30 @@ async function init() {
         wc: -1
     });
 
+    console.log('TON Public Key = ' + TonWeb.utils.bytesToHex(keyPair.publicKey));
+
     const myAddress = (await wallet.getAddress()).toString(false);
     console.log('TON Address = ' + myAddress);
 
     // ETH
 
-    const web3 = new Web3();
-    const account = web3.eth.accounts.create();
-    console.log('ETH Address = ' + account.address);
+    console.log('ETH Address = ' + ethAccount.address);
 
     const data = {
-        tonPrivateKey: newSecretKey,
-        ethPrivateKey: account.privateKey
+        tonPrivateKey: tonSecretKey,
+        ethPrivateKey: ethAccount.privateKey
     };
 
-    fs.writeFile(FILE_NAME, JSON.stringify(data), (err, data) => {
-        if (err) {
-            return console.error('Write file error', err);
-        }
+    if (needSave) {
+        fs.writeFile(FILE_NAME, JSON.stringify(data), (err, data) => {
+            if (err) {
+                return console.error('Write file error', err);
+            }
+            console.log('Done');
+        });
+    } else {
         console.log('Done');
-    });
+    }
 }
 
 init();
